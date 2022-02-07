06da247d3815089513025e9c411247d9449606ea
“anildalar” <dollorinfotech@gmail.com>
2022-02-07 10:00:30 +0530

    n

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ const config = require('./config.json')
 
 function App() {
   //1. State/ Hook Variables
-  const [student,setStudent] = useState([]);//Empty Array
+  const [student,setStudent] = useState({
+    data:[]
+  });//Empty Array
 
   //2. Functions defination
   let getStudents2 = (e)=>{
@@ -28,18 +30,20 @@ function App() {
         //AXIOS
 
         //What is the api
-        fetch(`${config.base_url}/api/students`).then((data)=>{
+        //Fetch API with Promise Chain
+        fetch(`${config.base_url}/api/friends`)
+        .then((data)=>{
           //let make data json readable
           return data.json();
         }).then((data)=>{
-          console.log(data.data);
+          console.log(data);
 
           //Set karne se pahle
           //console.log('before set',student);
           //not set the student data in student hook variable
-          setStudent(data.data);
+          setStudent(data);
           //Set karne ke baad data kya hai
-          //console.log('after set',student);
+
 
           //array.map(function(currentValue, index, arr));
 
@@ -58,27 +62,37 @@ function App() {
   return (
     <>
       <h1>Read Operation</h1>
-        <Button onClick={(e)=>{ getStudents(e) }}>Get My Students</Button>
-        <Button onClick={(e)=>{ getStudents2(e) }}>Get My Students2</Button>
+        <Button onClick={(e)=>{ getStudents(e) }}>Get My Friends</Button>
         <br />
         <br />
         <Table striped bordered hover>
           <thead>
             <tr>
               <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
+              <th>Friend Name</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
-            {}
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
+            {
+              student.data.map(function(currentValue, index, arr){
+                console.log(arr[index].id);
+                console.log(arr[index].attributes.name);
+                return (
+                    <tr key={index}>
+                      <td>{arr[index].id}</td>
+                      <td>{arr[index].attributes.name}</td>
+                      <td>
+                        <Button variant="success" size="sm">View</Button>&nbsp;
+                        <Button variant="primary" size="sm">Edit</Button>&nbsp;
+                        <Button variant="danger" size="sm">Delete</Button>
+                      </td>
+                    </tr>
+                )//JSX
+              })
+            }
+            
+
           </tbody>
         </Table>
     </>
